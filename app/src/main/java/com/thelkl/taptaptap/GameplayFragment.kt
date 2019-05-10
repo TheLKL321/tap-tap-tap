fa1f6a9eb22d5c769c16a7c6b93296b7435381cf
Łukasz Zarębski <lkl.zarebski@gmail.com>
2019-05-10 17:21:42 +0200

    add gameplay fragment

---
+++ app/src/main/java/com/thelkl/taptaptap/GameplayFragment.kt
@@ -0,0 +1,22 @@
+package com.thelkl.taptaptap
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GameplayFragment : Fragment() {
+    companion object {
+
+        fun newInstance(): GameplayFragment {
+            return GameplayFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.gameplay_fragment, container, false)
+    }
+}
