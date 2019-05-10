c872973ddf0c3ae09658249ee87ccf138f652d47
Łukasz Zarębski <lkl.zarebski@gmail.com>
2019-05-10 18:14:55 +0200

    set theme and show fragment on click

--- app/src/main/java/com/thelkl/taptaptap/MainActivity.kt
+++ app/src/main/java/com/thelkl/taptaptap/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.thelkl.taptaptap
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.view.View
+import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        // Hide the gameplay fragment
+        supportFragmentManager.beginTransaction().show(gameplayFragment).commit()
+    }
+
+    fun startGame(@Suppress("UNUSED_PARAMETER") view: View) {
+        supportFragmentManager.beginTransaction().show(gameplayFragment).commit()
     }
 }
