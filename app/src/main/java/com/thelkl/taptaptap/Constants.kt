97b400911c6ecffbc5301177b18c18ee3e1d7dc5
Łukasz Zarębski <lkl.zarebski@gmail.com>
2019-05-10 22:05:35 +0200

    add game start countdown

---
+++ app/src/main/java/com/thelkl/taptaptap/Constants.kt
@@ -0,0 +1,5 @@
+package com.thelkl.taptaptap
+
+// Unit of time is seconds
+const val START_COUNTDOWN_TIME = 3
+const val GAMEPLAY_COUNTDOWN_TIME = 5
--- app/src/main/java/com/thelkl/taptaptap/MainActivity.kt
+++ app/src/main/java/com/thelkl/taptaptap/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.thelkl.taptaptap
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.v4.app.FragmentActivity
 import android.view.View
 import kotlinx.android.synthetic.main.gameplay_fragment.*
@@ -8,18 +9,34 @@ import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : FragmentActivity() {
     private var taps = 0
+    private lateinit var startCountdownTimer: CountDownTimer
+    var currentTime: Double = START_COUNTDOWN_TIME.toDouble()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
         hideFragment(supportFragmentManager, gameplayFragment)
+
+        startCountdownTimer = object: CountDownTimer(START_COUNTDOWN_TIME.toLong() * 1000, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                startCountdownText.text = Math.ceil(currentTime).toInt().toString()
+                currentTime -= 0.1
+            }
+
+            override fun onFinish() {
+                startCountdownLayout.visibility = View.INVISIBLE
+            }
+        }
     }
 
     fun startGame(@Suppress("UNUSED_PARAMETER") view: View) {
         gameCountdownText.text = "5"
         gameMainText.text = "TAP"
         gameScoreText.text = "0"
+
+        startCountdownTimer.start()
+
         showFragment(supportFragmentManager, gameplayFragment)
     }
 
@@ -31,8 +48,15 @@ class MainActivity : FragmentActivity() {
     override fun onBackPressed() {
         if (gameplayFragment.isVisible) {
             hideFragment(supportFragmentManager, gameplayFragment)
-            taps = 0
+            resetGame()
         } else
             super.onBackPressed()
+    }
+
+    private fun resetGame() {
+        startCountdownTimer.cancel()
+        taps = 0
+        startCountdownLayout.visibility = View.VISIBLE
+        currentTime = START_COUNTDOWN_TIME.toDouble()
     }
 }
